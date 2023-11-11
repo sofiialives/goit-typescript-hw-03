dacc70765b6839eb1cff65c92d74e20b0fe0a516
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-11-11 15:58:30 +0200

    4 is done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,48 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  tenants: Person[] = [];
+
+  constructor(public key: Key) {
+    this.door = false;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened");
+    } else {
+      console.log("The door is still closed");
+    }
+  }
+}
+
+const key = new Key(Math.round(Math.random() * 1000));
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +50,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
